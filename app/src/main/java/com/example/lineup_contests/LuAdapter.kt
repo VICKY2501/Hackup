ba741feb25adc00414b1c836567034bd3274647d
[Vaibhav] <vg07000@gmail.com>
2022-01-06 18:39:40 +0530

    Layouts design Changed

--- app/src/main/java/com/example/lineup_contests/LuAdapter.kt
+++ app/src/main/java/com/example/lineup_contests/LuAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.lineup_contests
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
